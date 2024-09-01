d256b2c996414408f8bea76bccc23fbe5e370bd3
Sarunas <sarunas0329@gmail.com>
2024-09-01 16:23:30 +0300

    Fixed "/" having two toolbars

--- .Net + React/ReactApp/reactapp.client/src/Components/Home.jsx
+++ .Net + React/ReactApp/reactapp.client/src/Components/Home.jsx
@@ -1,44 +1,5 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { Button } from "@progress/kendo-react-buttons";
-import {
-  AppBar,
-  AppBarSection,
-  AppBarSpacer,
-  Avatar,
-} from "@progress/kendo-react-layout";
-
 const Home = () => {
-  const navigate = useNavigate();
-  return (
-    <div>
-      <React.Fragment>
-        <AppBar className="k-appbar">
-          <AppBarSection>
-            <h1 className="title">Užduotis</h1>
-          </AppBarSection>
-
-          <AppBarSection>
-            <ul>
-              <li>
-                <span onClick={() => navigate("/motherboard")}>
-                  Motherboards
-                </span>
-              </li>
-              <li>
-                <span onClick={() => navigate("/ramTypes")}>RAM Types</span>
-              </li>
-              <li>
-                <span onClick={() => navigate("/socketTypes")}>
-                  Socket Types
-                </span>
-              </li>
-            </ul>
-          </AppBarSection>
-        </AppBar>
-      </React.Fragment>
-    </div>
-  );
+  return;
 };
 
 export default Home;
